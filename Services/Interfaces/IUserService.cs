81a284ee936973fccbf16d6d6d79bb1a4f393028
Darko-Kolaric <darekolaric@gmail.com>
2025-10-18 22:18:04 +0200

    added user endpoint and authorization

---
+++ Services/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using Models.Dtos;
+using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IUserService
+    {
+        public Task Create(RegisterDto model);
+        public Task<User?> GetByUsername(string username);
+    }
+}
