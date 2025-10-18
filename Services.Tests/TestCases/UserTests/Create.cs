8bbb67c48feb7801286eaae8605d1220c42d2e28
Darko-Kolaric <darekolaric@gmail.com>
2025-10-18 22:32:25 +0200

    added user tests

---
+++ Services.Tests/TestCases/UserTests/Create.cs
@@ -0,0 +1,46 @@
+﻿using Models.Dtos;
+using Models.Entities;
+using Moq;
+using NUnit.Framework;
+using Repositories.Interfaces;
+using Services.Implementation;
+using Services.Interfaces;
+using Services.Tests.TestData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Tests.TestCases.UserTests
+{
+    public class Create
+    {
+        private IUserService _sut;
+        private Mock<IUserRepository> _userRep;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userRep = new Mock<IUserRepository>();
+            _userRep.Setup(x => x.SaveChanges()).Returns(Task.CompletedTask);
+            _sut = new UserService(_userRep.Object, AutoMapperConfig.GetMapper());
+        }
+
+        [Test]
+        public async Task GivenData_VerifyCreateCall()
+        {
+            var dto = new RegisterDto()
+            {
+                Email = "test@test.rs",
+                Password = "password",
+                Username = "username",
+            };
+
+            await _sut.Create(dto);
+
+            _userRep.Verify(x => x.Add(It.Is<User>(u => u.Username == u.Username && u.Password == dto.Password)), Times.Once);
+            _userRep.Verify(x => x.SaveChanges(), Times.Once);
+        }
+    }
+}
